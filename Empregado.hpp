00ead5da6399770c9728b52819990c359fb28b17
Vinícius Guimarães Silva <72668618+elferito@users.noreply.github.com>
2020-10-10 17:09:00 -0300

    Update Empregado.hpp

--- Empregado.hpp
+++ Empregado.hpp
@@ -4,23 +4,26 @@
 #include <iostream>
 #include <string>
 
+//magical number
+const double HORARIO_DIARIO_REGULAR = 8;
+const double PESO_HORAS_EXTRA = 0.5;
+
 class Empregado {
-	
-  public:
+  public: 
+
     double salarioHora;  
     double quotaMensalVendas;  
 
-
     double pagamentoMes(double horasTrabalhadas) {
  
-      double t = horasTrabalhadas;
+      double _horasTrabalhadas = horasTrabalhadas;
 	  
 	  //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
-      if (horasTrabalhadas > 8) {
-        double x = horasTrabalhadas - 8;
-        t += x / 2;
+      if (horasTrabalhadas > HORARIO_DIARIO_REGULAR) {
+        double horasExtras = horasTrabalhadas - HORARIO_DIARIO_REGULAR;
+        _horasTrabalhadas += horasExtras * PESO_HORAS_EXTRA;
       }
-	  return t * salarioHora;
+	    return _horasTrabalhadas * salarioHora;
     }
 	
 };
