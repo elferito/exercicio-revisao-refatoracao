fc5154a09f71f94e6fe3061a384039ea86779966
Vinícius Guimarães Silva <72668618+elferito@users.noreply.github.com>
2020-10-10 19:02:57 -0300

    Update Engenheiro.cpp

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -4,8 +4,33 @@
 class Engenheiro : public Empregado {
 
   public:
-    std::string nome;  
-	int projetos;
+
+    //Essa funçao seta o numero de projetos do engenheiro
+    void setProjetos(int _projetos) {
+
+      projetos = _projetos;
+
+    }
+
+    //Essa funçao geta o numero de projetos do engenheiro
+    int getProjetos() {
+
+      return projetos;
+
+    }
+  
+    //Essa funçao printa as informações a respeito do nome, salario e projetos do engenheiro
+    void printSaida(double mesSalario) {
+
+      std::cout << "Nome: " << getNome() << std::endl;
+      std::cout << "Salario Mes: " << pagamentoMes(mesSalario) << std::endl;
+      std::cout << "Projetos: " << getProjetos() << std::endl;
+      std::cout << std::endl;
+
+    }
+
+  private: 
+
+	  int projetos;
 	
 };
-
