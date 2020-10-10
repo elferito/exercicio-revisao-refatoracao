7edfacedde9619500db166468a047a8c6ef9a343
Vinícius Guimarães Silva <72668618+elferito@users.noreply.github.com>
2020-10-10 19:02:06 -0300

    Update Vendedor.cpp

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -1,16 +1,46 @@
 #include <string>
 #include "Empregado.hpp"
 
-//magical number
+//magical number: constante do numero de meses que um ano possui
 const double MESES_EM_ANO = 12;
 
 class Vendedor : public Empregado {
 
   public:
-    std::string nome;  	  
-	
+
+	//Essa funçao seta a quota de vendas mensais   
+    void setQuotaMensalVendas(double _quotaMensalVendas) {
+
+      quotaMensalVendas = _quotaMensalVendas;
+
+    }
+
+	//Essa funçao geta a quota de vendas mensais  
+	double getQuotaMensalVendas() {
+
+      return quotaMensalVendas;
+
+    }
+ 	  
+	//Essa funçao retorna a quota total anual do vendedor
 	double quotaTotalAnual() {
-	  return quotaMensalVendas * MESES_EM_ANO;
+
+	  return getQuotaMensalVendas() * MESES_EM_ANO;
+
 	}
-	
+
+	//Essa funçao printa as informações a respeito do nome, salario e projetos do vendedor
+    void printSaida(double mesSalario) {
+
+  	  std::cout << "Nome: " << getNome() << std::endl;
+  	  std::cout << "Salario Mes: " << pagamentoMes(mesSalario) << std::endl;  
+  	  std::cout << "Quota vendas: " << quotaTotalAnual() << std::endl;
+  	  std::cout << std::endl;
+
+    }
+
+  private:
+
+	double quotaMensalVendas;
+
 };
