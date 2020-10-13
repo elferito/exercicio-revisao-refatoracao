e3c59dd4a09aa3444025319e02a1cc2b9f7a47af
Vinícius Guimarães Silva <72668618+elferito@users.noreply.github.com>
2020-10-13 00:04:56 -0300

    Update Vendedor.cpp

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -9,34 +9,26 @@ class Vendedor : public Empregado {
   public:
 
 	//Essa funçao seta a quota de vendas mensais   
-    void setQuotaMensalVendas(double _quotaMensalVendas) {
-
-      quotaMensalVendas = _quotaMensalVendas;
-
-    }
+	void setQuotaMensalVendas(double _quotaMensalVendas) {
+	  quotaMensalVendas = _quotaMensalVendas;
+	}
 
 	//Essa funçao geta a quota de vendas mensais  
 	double getQuotaMensalVendas() {
-
-      return quotaMensalVendas;
-
-    }
+	  return quotaMensalVendas;
+	}
  	  
 	//Essa funçao retorna a quota total anual do vendedor
 	double quotaTotalAnual() {
-
 	  return getQuotaMensalVendas() * MESES_EM_ANO;
-
 	}
 
 	//Essa funçao printa as informações a respeito do nome, salario e projetos do vendedor
     void printSaida(double mesSalario) {
-
   	  std::cout << "Nome: " << getNome() << std::endl;
   	  std::cout << "Salario Mes: " << pagamentoMes(mesSalario) << std::endl;  
   	  std::cout << "Quota vendas: " << quotaTotalAnual() << std::endl;
   	  std::cout << std::endl;
-
     }
 
   private:
